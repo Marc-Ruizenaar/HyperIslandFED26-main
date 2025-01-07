7603b0263abf879a4fc895f875ac963b49c18818
Marc-Ruizenaar <info@marcruizenaar.com>
2025-01-07 16:44:34 +0100

    Updated with stopPropagation

--- 4/script.js
+++ 4/script.js
@@ -1,3 +1,4 @@
+const buttonContainer = document.getElementById("buttonContainer");
 const buttonChildContainer = document.getElementById("buttonChildContainer");
 
 const button2 = document.getElementById("button2");
@@ -5,20 +6,36 @@ const button3 = document.getElementById("button3");
 const button4 = document.getElementById("button4");
 const button5 = document.getElementById("button5");
 
-button2.onclick = function () {
+
+button2.onclick = function (e) {
+  e.stopPropagation();
   alert("Button 2 clicked!");
 };
 
-button3.addEventListener("click", () => {
+button3.addEventListener("click", (e) => {
+  e.stopPropagation();
   alert("Button 3 clicked!");
 });
 
 buttonChildContainer.addEventListener("click", (e) => {
-
+  e.stopPropagation();
   if (e.target.id === "button4") {
     alert("Button 4 clicked!");
   } else if (e.target.id === "button5") {
     alert("Button 5 clicked!");
   }
+});
 
+buttonContainer.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (e.target === buttonContainer) {
+    alert("Container clicked!");
+  }
+});
+
+buttonChildContainer.addEventListener("click", (e) => {
+  e.stopPropagation();
+  if (e.target === buttonChildContainer) {
+    alert("Child container clicked!");
+  }
 });
